2173154e4e35ab8d7117284f478454c9ee76e239
lee-vincent <vinnie@vinnielee.io>
2021-07-25 18:18:45 -0400

    initial eks cluster

---
+++ eks.tf
@@ -0,0 +1,42 @@
+
+module "eks" {
+  source                       = "terraform-aws-modules/eks/aws"
+  version                      = "17.1.0"
+  cluster_name                 = local.cluster_name
+  wait_for_cluster_interpreter = ["/bin/bash", "-c"]
+  cluster_version              = "1.20"
+  subnets                      = module.vpc.private_subnets
+
+  cluster_service_ipv4_cidr = "192.168.0.0/16"
+  enable_irsa               = false
+
+
+  cluster_endpoint_private_access_cidrs = []
+  cluster_endpoint_private_access_sg    = []
+  fargate_pod_execution_role_name       = ""
+  permissions_boundary                  = ""
+  vpc_id                                = module.vpc.vpc_id
+
+  worker_groups_launch_template = [
+    {
+      name                 = "worker-group-1"
+      instance_type        = "t2.small"
+      asg_desired_capacity = 1
+      asg_max_size         = 3
+      asg_min_size         = 1
+
+      tags = [
+        {
+          "key"                 = "k8s.io/cluster-autoscaler/enabled"
+          "propagate_at_launch" = "false"
+          "value"               = "true"
+        },
+        {
+          "key"                 = "k8s.io/cluster-autoscaler/${local.cluster_name}"
+          "propagate_at_launch" = "false"
+          "value"               = "true"
+        }
+      ]
+    }
+  ]
+}
--- variables.tf
+++ variables.tf
@@ -2,6 +2,7 @@ variable "region" {
   description = "AWS region"
   default     = "us-east-1"
 }
+
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t2.micro"
