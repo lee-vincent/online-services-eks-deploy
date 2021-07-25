61b9f965a7a05587ceaeeb0d1e46158d00c4ef31
lee-vincent <vinnie@vinnielee.io>
2021-07-25 17:22:52 -0400

    initial commit

---
+++ versions.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.51.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+  }
+
+  required_version = ">= 1.0.3"
+
+   backend "remote" {
+    hostname      = "app.terraform.io"
+    organization  = "vinnielee-io"
+
+    workspaces {
+      name = "online-services-eks-deploy"
+    }
+  }
+}
